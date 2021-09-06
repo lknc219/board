79e081ee94f4a2ae545c0f943e4c18198036253c
namChul <knc219@naver.com>
2021-09-06 17:49:14 +0900

    20210906 조금 수정

--- src/main/java/com/namChul/board/controller/BoardController.java
+++ src/main/java/com/namChul/board/controller/BoardController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
-import java.util.Iterator;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
